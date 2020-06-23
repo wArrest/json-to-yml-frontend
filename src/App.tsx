c5a47a38bcc3d5858329c8c33b40dc92377a2a86
mirror <mirrory@ciandt.com>
2020-06-23 09:31:25 +0800

    [Mirror] Layout adjustment.

--- src/App.tsx
+++ src/App.tsx
@@ -97,16 +97,40 @@ class App extends React.PureComponent<IProps, IState> {
           <h2>JSON和YML在线转换</h2>
           <br/>
           <p>无聊的在线小项目，源码可参考 <a href="https://github.com/wArrest/json-to-yml-frontend" target="_blank">github库</a></p>
-
-          <h3>YAML:</h3>
-          <p>YAML (YAML Ain't a Markup
-            Language)YAML不是一种标记语言，通常以.yml为后缀的文件，是一种直观的能够被电脑识别的数据序列化格式，并且容易被人类阅读，容易和脚本语言交互的，可以被支持YAML库的不同的编程语言程序导入，一种专门用来写配置文件的语言。可用于如：
-            Java，C/C++, Ruby, Python, Perl, C#, PHP等。</p>
-          <h3>JSON:</h3>
-          <p>
-            JSON(JavaScript Object Notation)是一种轻量级的数据交换格式，它基于JavaScript的一个子集，易于人的编写和阅读，也易于机器解析。
-            JSON采用完全独立于语言的文本格式，但是也使用了类似于C语言家族的习惯（包括C, C++, C#, Java, JavaScript, Perl, Python等）。 这些特性使JSON成为理想的数据交换语言。
-          </p>
+          <div className="title-content">
+            <h3>YAML:</h3>
+            <p>
+              <strong>YAML (YAML Ain't a Markup Language)</strong>
+              YAML不是一种标记语言，通常以
+              <code>.yml</code>
+              为后缀的文件，是一种直观的能够被电脑识别的数据序列化格式，并且容易被人类阅读，容易和脚本语言交互的，可以被支持YAML库的不同的编程语言程序导入，一种专门用来写配置文件的语言。可用于如：
+              <code>Java</code>，
+              <code>C/C++</code>，
+              <code>Ruby</code>，
+              <code>Python</code>，
+              <code>Perl</code>，
+              <code>C#</code>，
+              <code>PHP</code>等。
+            </p>
+          </div>
+          <div className="title-content">
+            <h3>JSON:</h3>
+            <p>
+              <strong>JSON(JavaScript Object Notation)</strong>
+              是一种轻量级的数据交换格式，它基于
+              <code>JavaScript</code>
+              的一个子集，易于人的编写和阅读，也易于机器解析。
+              JSON采用完全独立于语言的文本格式，但是也使用了类似于C语言家族的习惯（包括
+              <code>C</code>，
+              <code>C++</code>，
+              <code>C#</code>，
+              <code>Java</code>，
+              <code>JavaScript</code>，
+              <code>Perl</code>，
+              <code>Python</code>等）。
+              这些特性使JSON成为理想的数据交换语言。
+            </p>
+          </div>
         </div>
         <div className="json-editor"><h2>JSON <span>{this.state.jsonParseError}</span></h2>
           <AceEditor
